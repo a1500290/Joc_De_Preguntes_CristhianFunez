9baba4dd966c06e8fcc60ad6d48b369d1e28960e
Cristhian Funez <79807616+a1500290@users.noreply.github.com>
2021-02-28 22:07:21 +0100

    Update Program.cs

--- Joc_De_Preguntes_CristhianFunez/Program.cs
+++ Joc_De_Preguntes_CristhianFunez/Program.cs
@@ -8,8 +8,33 @@ namespace Joc_De_Preguntes_CristhianFunez
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Clear();
+            string tema;
+            Console.WriteLine("Juego de preguntas");
+            //pas 1
+            tema = escollirTema(); 
+
+
+        }
+        
+        static string escollirTema()
+        {
+            string tema="", opcio;
+            Console.WriteLine("escoge una opcio o prem 'Q' per surti");
+            Console.WriteLine("1 - Geografia");
+            Console.WriteLine("2 - Arte y literatura");
+            Console.WriteLine("3 - Micelánea");
+            do
+            {
+                opcio = Console.ReadLine();
+                switch (opcio)
+                {
+                    
+                }
+            } while (opcio == "");
+            return tema;
         }
     }
 }
